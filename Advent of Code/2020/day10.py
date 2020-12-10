25a3a15938993830c6e65c91f1ebb89053aadcf4
steveman1123 <31524228+steveman1123@users.noreply.github.com>
2020-12-10 10:14:00 -0600

    Update day10.py

--- Advent of Code/2020/day10.py
+++ Advent of Code/2020/day10.py
@@ -1,6 +1,6 @@
 with open('day10in.txt','r') as f:
   t = f.read().split('\n')
-  t = [int(e) for e in t if len(e)>0]
+  t = [int(e) for e in t if len(e)>0] #convert numbers from str to int
 
 t.sort() #sort in ascending order
 #get the differences between each element
@@ -17,10 +17,10 @@ pt1 = ones*threes #pt1 wants the multiple
 
 #the idea for the following is that we looks for groups of 1's that can be converted to higher orders, so any group of 2 or more 1's
 
-oneGroups = [] #list of lengths of groups of 1's (more 1 individual 1)
+oneGroups = [] #list of lengths of groups of 1's (more than one individual 1)
 i=0 #index of the diff
 while i < len(diff)-1:
-  grplen=1
+  grplen=1 #length of the group of 1's
   while(diff[i]==diff[i+1]==1): #if there's a group
     grplen+=1 #figure out how big the group is
     i+=1
@@ -36,3 +36,18 @@ pt2 = reduce(lambda a,b:a*b,alts) #multiply together to get the total combinatio
 
 print pt1
 print pt2
+
+'''
+For example:
+0,1,2,3,6
+has a diff of
+1,1,1,3
+which makes it a group of 3
+That means there are combinations of:
+0,1,2,3,6
+0,2,3,6
+0,1,3,6
+0,3,6
+That's 4 unique groups and represented in 3*e-5
+But since a group of 2 doesn't follow the same rule, it just has 2 combinations, so that can be represented as max(3*e-5,2)
+'''
