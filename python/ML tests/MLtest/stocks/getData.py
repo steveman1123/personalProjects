3b29df1f694ef7f44f87e4252964397978e1b605
Steve <smw1109@protonmail.com>
2021-12-30 22:47:45 -0600

    add stock stuff

--- python/ML tests/MLtest/stocks/getData.py
+++ python/ML tests/MLtest/stocks/getData.py
@@ -20,7 +20,8 @@ else:
 #dependent value 1 is t/f price>last week avg
 #dependent value 2 is t/f price>yesterday close3
 def genData(symb,verbose=True):
-  numDays=1000 #appx number of trade days to get the history for (does not account for holidays)
+  numDays=500 #appx number of trade days to get the history for (does not account for holidays)
+  fromdate=str(wd(dt.date.today(),-numDays)) #approximate number of trade days ago
   maxNews = 150 #maximum number of news articles/headlines to pull in
   df_out = pd.DataFrame() #init the output dataframe
   if(verbose):
@@ -29,8 +30,7 @@ def genData(symb,verbose=True):
   
   #price history
   if(verbose): print("getting history")
-  fromdate=str(wd(dt.date.today(),-numDays)) #approximate number of trade days ago
-  hist = n.getQuote(assetclass="stocks",symb=symb,data="historical",limit=numDays)
+  hist = n.getQuote(assetclass="stocks",symb=symb,data="historical",limit=numDays,fromdate=fromdate)
   #convert from json to csv
   hist=hist['data']['tradesTable']['rows'] #list of dicts
   # convert to dataframe and convert strings to numbers
@@ -44,11 +44,14 @@ def genData(symb,verbose=True):
   if(verbose): print("getting dividends")
   divs = n.getQuote(symb=symb,assetclass="stocks",data="dividends")
   df_divs = pd.DataFrame(divs['data']['dividends']['rows'])
-
+  
+  '''
+  #removed due to redundancy found in earnings surprise
   if(verbose): print("getting eps")
   eps = n.getQuote(symb=symb,assetclass="stocks",data="eps")
   df_eps = pd.DataFrame(eps['data']['earningsPerShare'])
-
+  '''
+  
   if(verbose): print("getting short interest")
   si = n.getQuote(symb=symb,assetclass="stocks",data="short-interest")
   df_si = pd.DataFrame(si['data']['shortInterestTable']['rows'])
@@ -73,10 +76,12 @@ def genData(symb,verbose=True):
   intrades = n.getCompany(symb=symb,data="insider-trades",limit=int(intrades['data']['transactionTable']['totalRecords'])) #use the total number of trades to request all of them
   df_intrades = pd.DataFrame(intrades['data']['transactionTable']['table']['rows'])
   
+  '''
   if(verbose): print("getting news")
   news = n.getNews(symb=symb,assetclass="stocks",limit=1)
   news = n.getNews(symb=symb,assetclass="stocks",limit=min(int(news['data']['totalrecords']),maxNews))
   df_news = pd.DataFrame(news['data']['rows'])
+  '''
   
   if(verbose): print("getting target price history")
   tgtp = n.getAnalyst(symb,"targetprice")
@@ -84,10 +89,8 @@ def genData(symb,verbose=True):
   
   #get the nasdax index
   if(verbose): print("getting nasdaq quote")
-  ndx = n.getQuote(symb="ndx",assetclass="index",data="historical",limit=numDays) #nasdaq index
+  ndx = n.getQuote(symb="ndx",assetclass="index",data="historical",limit=numDays,fromdate=fromdate) #nasdaq index
   df_ndx = pd.DataFrame(ndx['data']['tradesTable']['rows']) #convert to dataframe
-  
-  print(df_hist)
   
   #calculate additional columns from historical
   if(verbose): print("calculating vwap")
@@ -103,10 +106,101 @@ def genData(symb,verbose=True):
   if(verbose): print("calculating obv")
   obv = p.obv(df_hist) #on-balance volume
   
-  #combine data into dataframe
+  #prep the data so that it'll fit into the dataframe
   if(verbose): print("cleaning data")
+  # hist - baseline (should have the most data points)
+  print("hist")
+  print(df_hist)
+  # divs
+  #should transform from cols of exDate,amt,decDate,recDate,pmtDate to 2 cols of amt and divDates where 0=not important, 1=ex,2=dec,3=rec,4=pmt
+  #ignore the type col, dates should also be transformed to date rather than string
+  print("\ndivs")
+  print(df_divs)
+  '''
+  # eps
+  #omit. All useful data can be found in df_es
+  print("\neps")
+  print(df_eps)
+  '''
+  # si
+  #cols should be converted to numeric (date strings to dates)
+  #add rows to complete to hist size
+  print("\nsi")
+  print(df_si)
+  # es
+  #rm fiscalQtrEnd col
+  #ensure cols are numeric (should be already)
+  #convert date string to date
+  #add rows to complete to hist size
+  print("\nes")
+  print(df_es)
+  # income
+  #transform to have many columns, current value2-5 should be quarter-end dates gathered from the headers (before isolating to rows)
+  #ensure data is numeric
+  #add rows to complete to hist size
+  print("\nincome")
+  print(df_incomeStatement)
+  # balance
+  #same as income
+  print("\nbalance")
+  print(df_balanceSheet)
+  # cash
+  #same as income
+  print("\ncash flow")
+  print(df_cashFlow)
+  # ratios
+  #same as income
+  print("\nratios")
+  print(df_finRatios)
+  # intrades
+  #rm url col
+  #convert names, relations, xtn type, own type to numeric (based on uniqueness)
+  #ensure data is numeric (and date strings are dates)
+  print("\nintrades")
+  print(df_intrades)
+  '''
+  # news - omit for now (will have to make a secondary one for sentiment analysis of the headlines)
+  #rm url,id,imagedomain,created,image,publisher cols
+  #convert ago to date
+  print("\nnews")
+  print(df_news)
+  '''
+  # tgtp
+  #isolate to z (ignore x & y)
+  #add rows to complete to hist
+  #added rows of buy/hold/sell should have values of -1
+  #TODO: might want to consider experimenting with having the added rows have the previous value (so if 1/1 is 20, then 1/2-1/31 should also be 20 instead of -1)
+  #convert consensus to numeric (based on uniquness)
+  print("\ntgtp")
+  print(df_tgtp)
+  # ndx
+  #ensure numbers are numeric (also rename cols to differentiate from target asset)
+  print("\nndx")
+  print(df_ndx)
   
+  '''
+  #calculated ones should be fine (just be appended to the output)
+  # vwap
+  print("\nvwap")
+  print(vwap)
+  # vpt
+  print("\nvpt")
+  print(vpt)
+  # ema
+  print("\nema")
+  print(ema)
+  # sma
+  print("\nsma")
+  print(sma)
+  # delta
+  print("\ndelta")
+  print(delta)
+  # obv
+  print("\nobv")
+  print(obv)
+  '''
   
+  #combine data into dataframe
   if(verbose): print("combining into single dataframe")
   
   
