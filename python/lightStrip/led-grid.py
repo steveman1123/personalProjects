bb9f9bd1a439568e82709712482da7b109b24c52
steve <smw1109@protonmail.com>
2022-01-03 23:01:55 -0600

    update lights

--- python/lightStrip/led-grid.py
+++ python/lightStrip/led-grid.py
@@ -39,21 +39,55 @@ def scrollText(text,scrollSpeed):
   return False
 
 
+#flash characters one at a time on the grid
+#where text is a string of valid characters
+#color is the text color
+#back color is the background color
+#ontime is how long each char should be displayed for (in sec)
+#offtime is how long of a wait between characters
+def dispChars(text,color,backcolor,ontime=0.25,offtime=0.05):
+  for char in text.lower(): #esure lowercase since CHARS4X4 is all in lowercase
+    chargrid = [e['grid'].split("|") for e in CHARS4X4 if e['char']==char][0]
+    if(len(chargrid)>0): #only display if the character is valid, else skip it as if it wasn't there in the first place
+      for c,col in enumerate(chargrid):
+        for r,px in enumerate(col):
+          if(int(px)):
+            pixel_framebuf.pixel(r,pixel_width-1-c,color)
+          else:
+            pixel_framebuf.pixel(r,pixel_width-1-c,backcolor)
+      pixel_framebuf.display()
+      print(char)
+      time.sleep(ontime)
+      pixel_framebuf.fill(0x000000)
+      pixel_framebuf.display()
+      time.sleep(offtime)
 
 
 def main():
   
   while True:
+    dispChars("the quick brown fox jumps over the lazy dog.  ",0x0000FF,0x000000)
+
+  '''
+  #test scrolling
+  while True:
     pixel_framebuf.pixel(0,0,0xFF0000)
     for col in range(pixel_width):
       pixel_framebuf.scroll(1,0)
+      tmpbuf = pixel_framebuf
+      pixel_framebuf.fill(0x000000)
+      pixel_framebuf = tmpbuf
       pixel_framebuf.display()
+      time.sleep(1)
     for row in range(pixel_height):
       pixel_framebuf.scroll(0,1)
       pixel_framebuf.display()
+      time.sleep(1)
       
-  #while True:
-    '''
+  '''
+  '''
+  #initial test
+  while True:
     for col in range(pixel_width):
       for row in range(pixel_height):
         pixel_framebuf.fill(0x000000)
