0e7e32ba8b483c2cce4b521641deaf6e6c331d98
steve <smw109@protonmail.com>
2023-01-05 23:31:39 -0600

    update script

--- scripts/bash/updateRepos.sh
+++ scripts/bash/updateRepos.sh
@@ -1,26 +1,35 @@
 #update all repos in a specified directory (relative or absolute)
 
 #specify directory
-gitdir="."
+gitdir=".";
 
 #loop through all objects in dir
 for i in $gitdir/* ; do
   #if directory
   if [ -d "$i" ]; then
     #tell us
-    echo "$i"
+    echo "$i";
     #move into it
-    cd "$i"
-    #echo "$(pwd -P)"
+    cd "$i";
+    #echo "$(pwd -P)";
     #get all hidden directories
-    dirs=$(ls -ad ./.*/)
+    dirs=$(ls -ad ./.*/);
     #if .git/ is contained in the hidden directories
     if  [[ "$dirs" == *".git/"* ]]; then
       #echo "pulling"
-      #add the directory if necessary (firs run)
-      #git config --global --add safe.directory "$(pwd -P)"
-      #pull the new version
-      git pull
+      #attempt to pull the new version
+      pulldata=$(git pull);
+      echo $pulldata
+      #add the directory if necessary (first run)
+      #TODO: this part isn't tested
+      if [[ $pulldata == *"git config --global --add safe.directory"* ]]; then
+        echo "adding new safe directory";
+        git config --global --add safe.directory "$(pwd -P)"
+        echo "attempting to pull again";
+        git pull
+      fi
+
+      
     else
       echo "Not a git dir"
     fi
