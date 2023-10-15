bcbe57f082ae542a653772555f5438569844d616
steve <smw1109@protonmail.com>
2023-10-15 08:14:15 -0700

    finish adding functionality

--- scripts/bash/filename2id3.sh
+++ scripts/bash/filename2id3.sh
@@ -40,7 +40,7 @@ echo album: $album;
 #TODO: add a prompt to ask to delete all previous data (id3v2 -D)
 #TODO: prompt for year and genre, then write if it's not null
 
-echo -n "delete all existing metadata? (y/N) "
+echo -n "delete all existing tags? (y/N) "
 read deleteall;
 
 echo $deleteall;
@@ -58,22 +58,22 @@ fi
 echo -n "release year: "
 read year;
 
-if [[ ${#year} -eq 4 ]] then
-  #echo "year is $year"
-else
-  #echo "year not valid"
-fi
+#if [[ ${#year} -eq 4 ]] then
+#  echo "year is $year"
+#else
+#  echo "year not valid"
+#fi
 
 
 id3v2 -L
 echo -n "album genre number: "
 read genreNum;
 
-if [[ $genreNum -gt 0 ]] then
-  #echo "genre is $(id3v2 -L | grep $genreNum)"
-else
-  #echo "bad genre"
-fi
+#if [[ $genreNum -gt -1 ]] then
+#  echo "genre is $(id3v2 -L | grep $genreNum)"
+#else
+#  echo "bad genre"
+#fi
 
 echo -e
 echo -e
@@ -87,33 +87,49 @@ echo "genre: $(id3v2 -L | grep $genreNum)"
 echo -n "continue? (Y/n)"
 read okgo
 
+if [[ ${okgo,,} == "n" ]] then
+  okgo="n";
+else
+  okgo="y";
+fi
 
-#for every mp3
-for f in "$dir"/*"$fileext"; do
-  #split by / and get the filename
-  IFS="/" filename=($f);
-  filename=${filename[-1]};
-  IFS=$ogifs;
-  
-  #split filename by delim and get the track and title
-  tmp=$filename$delim;
-  trackandtitle=();
-  while [[ $tmp ]]; do
-    trackandtitle+=( "${tmp%%"$delim"*}" );
-    tmp=${tmp#*"$delim"};
+if [[ $okgo == "y" ]] then
+
+  if [[ $deleteall == "y" ]] then
+    echo "deleting existing tags";
+    id3v2 -D
+  fi
+
+  #for every mp3
+  for f in "$dir"/*"$fileext"; do
+    #split by / and get the filename
+    IFS="/" filename=($f);
+    filename=${filename[-1]};
+    IFS=$ogifs;
+    
+    #split filename by delim and get the track and title
+    tmp=$filename$delim;
+    trackandtitle=();
+    while [[ $tmp ]]; do
+      trackandtitle+=( "${tmp%%"$delim"*}" );
+      tmp=${tmp#*"$delim"};
+    done
+
+    #isolate the track
+    track=${trackandtitle[0]};
+    #trim the file extension from the filename to get the title
+    title=${trackandtitle[1]:0:${#trackandtitle[1]}-${#fileext}};
+    echo $track, $title;
+
+    #write the tags
+    id3v2 -T "$track" "$f";
+    id3v2 -t "$title" "$f";
+    id3v2 -a "$artist" "$f";
+    id3v2 -A "$album" "$f";
+    id3v2 -g "$genreNum" "$f";
+    id3v2 -y "$year" "$f";
   done
 
-  #isolate the track
-  track=${trackandtitle[0]};
-  #trim the file extension from the filename to get the title
-  title=${trackandtitle[1]:0:${#trackandtitle[1]}-${#fileext}};
-  echo $track, $title;
-
-  #write the tags
-  id3v2 -T "$track" "$f";
-  id3v2 -t "$title" "$f";
-  id3v2 -a "$artist" "$f";
-  id3v2 -A "$album" "$f";
-done
-
-
+else
+  echo "cancelling";
+fi
